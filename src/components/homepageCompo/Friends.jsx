dddfa59fb209a697aa368dd11dc3f7a476023252
abusayadebu <abusayad743@gmail.com>
2026-04-17 17:02:00 +0600

    friendCard reorganised and sorted

--- src/components/homepageCompo/Friends.jsx
+++ src/components/homepageCompo/Friends.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import FriendCard from '../../ui/FriendCard';
 
 const Friends = () => {
 
@@ -20,59 +21,7 @@ const Friends = () => {
 
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10'>
                 {
-                    friends.map(friend => {
-                        const {
-                            id,
-                            name,
-                            picture,
-                            email,
-                            days_since_contact,
-                            status,
-                            tags,
-                            bio
-                        } = friend;
-
-                        return (
-                            <div key={id} className="card bg-base-100 shadow-md p-6">
-
-                                {/* Image */}
-                                <div className="flex justify-center">
-                                    <img
-                                        src={picture}
-                                        alt={name}
-                                        className="w-[80px] h-[80px] rounded-full"
-                                    />
-                                </div>
-
-                                {/* Info */}
-                                <div className="text-center mt-3">
-                                    <h2 className="font-semibold text-lg">{name}</h2>
-                                    <p className="text-sm text-gray-500">
-                                        {days_since_contact}d ago
-                                    </p>
-                                </div>
-
-                                {/* Tags */}
-                                <div className="flex flex-wrap justify-center gap-2 mt-2">
-                                    {
-                                        tags.map((tag, index) => (
-                                            <span key={index} className="badge bg-green-200 font-semibold uppercase py-4 rounded-2xl">
-                                                {tag}
-                                            </span>
-                                        ))
-                                    }
-                                </div>
-
-                                {/* Status */}
-                                <div className="flex justify-center mt-3">
-                                    <span className={`badge font-semibold uppercase py-4 rounded-2xl text-white ${status === "overdue" ? "badge-error" : status === "almost due" ? "badge-warning" : "badge-success"}`}>
-                                    {status}
-                                    </span>
-                                </div>
-
-                            </div>
-                        );
-                    })
+                    friends.map(friend => <FriendCard key={friend.id} friend={friend}></FriendCard>)
                 }
             </div>
         </div>
---
+++ src/ui/FriendCard.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const FriendCard = ({friend}) => {
+                     const {
+                            id,
+                            name,
+                            picture,
+                            email,
+                            days_since_contact,
+                            status,
+                            tags,
+                            bio
+                        } = friend;
+                 return (
+                    <div className="card bg-base-100 shadow-md p-6">
+
+                                {/* Image */}
+                                <div className="flex justify-center">
+                                    <img
+                                        src={picture}
+                                        alt={name}
+                                        className="w-[80px] h-[80px] rounded-full"
+                                    />
+                                </div>
+
+                                {/* Info */}
+                                <div className="text-center mt-3">
+                                    <h2 className="font-semibold text-lg">{name}</h2>
+                                    <p className="text-sm text-gray-500">
+                                        {days_since_contact}d ago
+                                    </p>
+                                </div>
+
+                                {/* Tags */}
+                                <div className="flex flex-wrap justify-center gap-2 mt-2">
+                                    {
+                                        tags.map((tag, index) => (
+                                            <span key={index} className="badge bg-green-200 font-semibold uppercase py-4 rounded-2xl">
+                                                {tag}
+                                            </span>
+                                        ))
+                                    }
+                                </div>
+
+                                {/* Status */}
+                                <div className="flex justify-center mt-3">
+                                    <span className={`badge font-semibold uppercase py-4 rounded-2xl text-white ${status === "overdue" ? "badge-error" : status === "almost due" ? "badge-warning" : "badge-success"}`}>
+                                    {status}
+                                    </span>
+                                </div>
+
+                            </div>
+                        );
+                    };
+
+    export default FriendCard;
